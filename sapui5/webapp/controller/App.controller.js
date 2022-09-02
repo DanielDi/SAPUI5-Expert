80e637e8c1e19426a4c1a4d517fa73df308743d8
DanielDi <despinalm@unal.edu.co>
2022-09-02 23:05:38 +0000

    fragment

--- sapui5/webapp/controller/App.controller.js
+++ sapui5/webapp/controller/App.controller.js
@@ -1,21 +1,13 @@
 // @ts-nocheck
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
+    "sap/ui/core/mvc/Controller"
 ],
     /**
      * @param{typeof sap.ui.core.mvc.Controller} Controller
-     * @param{typeof sap.m.MessageToast} MessageToast
      */
-    function (Controller, MessageToast) {
+    function (Controller) {
         return Controller.extend("dtt.sapui5.controller.App", {
             onInit: function () {
-            },
-            onShowHello: function () {
-                var oBundle = this.getView().getModel("i18n").getResourceBundle();
-                var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-                var sMSG = oBundle.getText("helloMsg", [sRecipient]);
-                MessageToast.show(sMSG);
             },
         }
         );
